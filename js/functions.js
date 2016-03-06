0f0799de627ad83776c00fff9e21c38654f1f973
Jason <jdesigns.jason@gmail.com>
2016-03-06 15:19:01 -0600

    form tweaks, added code snippets, js coderun addition for tabs to spaces

--- js/functions.js
+++ js/functions.js
@@ -47,7 +47,7 @@ $(document).ready(function() {
     var snippets = document.querySelectorAll('.code-sample');
     for (var i = 0; i < snippets.length; i++) {
       var newSnippet = snippets[i].innerHTML;
-      newSnippet = newSnippet.replace(/\</g, '&lt;').replace(/\>/g, '&gt;');
+      newSnippet = newSnippet.replace(/\</g, '&lt;').replace(/\>/g, '&gt;').replace(/\t/g, '&nbsp;&nbsp;');
       snippets[i].innerHTML = newSnippet;
     }
   }; codeRun();
